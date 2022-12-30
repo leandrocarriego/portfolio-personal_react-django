dec1b21ac58adfd62313a7479d1fe2768648cb05
Leandro Carriego <leandrocarriego@hotmail.com>
2022-12-29 22:56:28 -0300

    routes y redux config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
+import Home from "containers/pages/Home";
+import Projects from "containers/pages/Projects";
+import Error404 from "containers/errors/Error404";
 
 function App() {
   return (
-    <div className="">
-      <h1>Portfolio
-      </h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="*" element={<Error404/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/projects/" element={<Projects/>} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
